556cc68a0e74eaa5289497457a5ec2e55a7a779c
Ayan Sarkar <94477087+AyanSarkar85@users.noreply.github.com>
2023-09-13 01:04:38 +0530

    Domain layer changes
    Changes added for model class and interface

---
+++ ApiWithCleanArchitecture/Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+
+namespace Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product? GetProductById(int productId);
+        void InsertProduct(Product product);
+        void DeleteProduct(int productId);
+        void UpdateProduct(Product product);
+        void Save();
+    }
+}
