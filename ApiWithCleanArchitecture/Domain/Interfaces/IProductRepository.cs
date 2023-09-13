ba4d73ba9eb6d767ffc4975266c7a9d4f1bc7551
Ayan Sarkar <94477087+AyanSarkar85@users.noreply.github.com>
2023-09-14 00:55:42 +0530

    Layer changes
    Infra changes added

--- ApiWithCleanArchitecture/Domain/Interfaces/IProductRepository.cs
+++ ApiWithCleanArchitecture/Domain/Interfaces/IProductRepository.cs
@@ -5,7 +5,7 @@ namespace Domain.Interfaces
     public interface IProductRepository
     {
         IEnumerable<Product> GetProducts();
-        Product? GetProductById(int productId);
+        //Product? GetProductById(int productId);
         void InsertProduct(Product product);
         void DeleteProduct(int productId);
         void UpdateProduct(Product product);
---
+++ ApiWithCleanArchitecture/Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Interfaces;
+using Domain.Models;
+using Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public readonly ProductDbContext _dbContext;
+        
+        public ProductRepository(ProductDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Product? GetProductByID(int productId)
+        {
+            return _dbContext.Products.Find(productId);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            _dbContext.Products.Add(product);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _dbContext.Entry(product).State = EntityState.Modified;
+            Save();
+        }
+
+        public void DeleteProduct(int ProductId)
+        {
+            var product = _dbContext.Products.Find(ProductId);
+            if (product != null)
+            {
+                _dbContext.Products.Remove(product);
+                Save();
+            }
+        }        
+    }
+}
